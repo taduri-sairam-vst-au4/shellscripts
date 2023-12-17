18c6c3f9d9ead36b69f34eedff64f50004637fc7
sairam <sairam.sai.t7@gmail.com>
2023-12-17 11:34:27 +0530

    mysql installatiom

---
+++ functions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+INSTALLATION(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 failed"
+        exit 1
+    else
+        echo "$2 success"
+    fi
+}
+
+if [ $ID -ne 0]
+then
+    echo "not a root user"
+    exit 1
+else
+    echo "root user"
+fi
+
+yum install git -y
+INSTALLATION $? "Git"
